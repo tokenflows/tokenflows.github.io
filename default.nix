84596a396b17f63a3e7a5730bad0f651ecf3e2d6
wolstaeb <82072426+wolstaeb@users.noreply.github.com>
2024-04-15 14:08:01 +1200

    CONFIG: Crowdin configuration and added licences

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 tokenflows <https://tokenflows.xyz/>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   locked = lock.nodes.flake-compat.locked;
